6e407e4bc438523109d8cb57c29ec964199113d7
James Wilding <you@example.comJamesWildinggit config --global user.name JamesWildinggit config --global user.name James>
2022-11-22 09:53:35 +0000

    dynamic film shown

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import Movies from './components/Movies/Movies';
 const App = () => {
   const [movies, setMovies] = useState([]);
   const getMovies = async () => {
-    const url = 'https://api.tvmaze.com/search/shows?q=girls';
+    const url = 'https://api.tvmaze.com/search/shows?q=the';
     const response = await fetch(url);
     const responseJSON = await response.json();
     setMovies(responseJSON);
-    console.log(responseJSON)
+    console.log(responseJSON);
   };
   useEffect(() => {
     getMovies();
@@ -21,7 +21,6 @@ const App = () => {
       <NavBar/>
       <div className='app'>
         <Movies movies={movies}/>
-
       </div>
     </>
   );

8c3458e07a6b53273ad5eb1cce20f12b81ece1e1
Jawad Ahmed <jawad@mahmed.co.uk>
2022-11-22 12:07:25 +0000

    Merge pull request #1 from James/j-ahmed007

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import NavBar from './components/NavBar/NavBar';
+import SearchBar from './components/SearchBar/SearchBar';
 import Movies from './components/Movies/Movies';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const url = 'http://www.omdbapi.com/?s=avengers&apikey=b4e8a7d8';
+  const [searchValue, setSearchValue] = useState('');
+
+  const getMovies = async (searchValue) => {
+    const url = `https://api.tvmaze.com/search/shows?q=${searchValue}`;
     const response = await fetch(url);
     const responseJSON = await response.json();
-    setMovies(responseJSON.Search);
+    setMovies(responseJSON);
   };
+
   useEffect(() => {
-    getMovies();
-  }, []);
+    getMovies(searchValue);
+  }, [searchValue]);
 
   return (
     <>
       <NavBar/>
       <div className='app'>
+        <SearchBar searchValue={searchValue} setSearchValue={setSearchValue}/>
         <Movies movies={movies}/>
       </div>
     </>
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,12 +1,20 @@
 import './Movies.css';
-import React from 'react';
+import React, { useState } from 'react';
+import MoviesInfo from './MoviesInfo';
 
 const Movies = (props) => {
+    const [movieInfo, setmovieInfo] = useState(false);
+    const toggleInfo = event => {
+        setmovieInfo(!movieInfo);
+    };
+
     return (
         <div>
             <div className='movies'>
                 <h2 className='movies-popular-title'>Popular Movies<hr></hr></h2>
-                {props.movies.map((movie, index) => <img className='movie-poster' src={movie.Poster}></img>)}
+                {props.movies.map((movie) => <div><img className='movie-poster' src={movie.show.image.medium} alt={movie.movie} onClick={(toggleInfo)}>
+                </img>{movieInfo ? <MoviesInfo movie={movie}/> : ""}</div>
+                )};
             </div>
         </div>
     );

604f4ba4a38dd0e6d588997dcee853bb7db2bacd
Jawad Ahmed <jawad@mahmed.co.uk>
2022-12-06 09:59:49 +0000

    seventh commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,131 @@
+import {useEffect, useState} from "react";
 import './App.css';
-import React, { useEffect, useState } from 'react';
-import NavBar from './components/NavBar/NavBar';
-import SearchBar from './components/SearchBar/SearchBar';
-import Movies from './components/Movies/Movies';
+import Movie from "./components/Movie";
+import axios from 'axios';
+import Youtube from 'react-youtube';
 
-const App = () => {
-  const [movies, setMovies] = useState([]);
+function App() {
+    const MOVIE_API = "https://api.themoviedb.org/3/";
+    const SEARCH_API = MOVIE_API + "search/movie";
+    const DISCOVER_API = MOVIE_API + "discover/movie";
+    const API_KEY = "e37d0e8e90dcbab150e4c6dc6071a88c";
+    const BACKDROP_PATH = "https://image.tmdb.org/t/p/w1280";
 
-  const [searchValue, setSearchValue] = useState('');
+    const [playing, setPlaying] = useState(false);
+    const [trailer, setTrailer] = useState(null);
+    const [movies, setMovies] = useState([]);
+    const [searchKey, setSearchKey] = useState("");
+    const [movie, setMovie] = useState({title: "Loading Movies..."});
 
-  const getMovies = async (searchValue) => {
-    const url = `https://api.tvmaze.com/search/shows?q=${searchValue}`;
-    const response = await fetch(url);
-    const responseJSON = await response.json();
-    setMovies(responseJSON);
-  };
+    useEffect(() => {
+        fetchMovies();
+    }, []);
 
-  useEffect(() => {
-    getMovies(searchValue);
-  }, [searchValue]);
+    const fetchMovies = async (event) => {
+        if (event) {
+            event.preventDefault();
+        };
 
-  return (
-    <>
-      <NavBar />
-      <div className='app'>
-        <SearchBar searchValue={searchValue} setSearchValue={setSearchValue}/>
-        <Movies movies={movies}/>
-      </div>
-    </>
-  );
+        const {data} = await axios.get(`${searchKey ? SEARCH_API : DISCOVER_API}`, {
+            params: {
+                api_key: API_KEY,
+                query: searchKey
+            }
+        });
+
+        setMovies(data.results);
+        setMovie(data.results[0]);
+
+        if (data.results.length) {
+            await fetchMovie(data.results[0].id);
+        };
+    };
+
+    const fetchMovie = async (id) => {
+        const {data} = await axios.get(`${MOVIE_API}movie/${id}`, {
+            params: {
+                api_key: API_KEY,
+                append_to_response: "videos"
+            }
+        });
+
+        if (data.videos && data.videos.results) {
+            const trailer = data.videos.results.find(vid => vid.name === "Official Trailer");
+            setTrailer(trailer ? trailer : data.videos.results[0]);
+        };
+
+        setMovie(data);
+    };
+
+    const selectMovie = (movie) => {
+        fetchMovie(movie.id);
+        setPlaying(false);
+        setMovie(movie);
+        window.scrollTo(0, 0);
+    };
+
+    const renderMovies = () => (
+        movies.map(movie => (
+            <Movie selectMovie={selectMovie} key={movie.id} movie={movie}/>
+        ))
+    );
+
+    return (
+        <div className="App">
+            <header className="center-max-size header">
+                <span className={"brand"}>IMDB Web App</span>
+                <form className="form" onSubmit={fetchMovies}>
+                    <input className="search" type="text" id="search" onInput={(event) => setSearchKey(event.target.value)}/>
+                    <button className="submit-search" type="submit"><i className="fa fa-search"></i></button>
+                </form>
+            </header>
+            {movies.length ?
+                <main>
+                    {movie ?
+                        <div className="poster"
+                             style={{backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url(${BACKDROP_PATH}${movie.backdrop_path})`}}>
+                            {playing ?
+                                <>
+                                    <Youtube
+                                        videoId={trailer.key}
+                                        className={"youtube amru"}
+                                        containerClassName={"youtube-container amru"}
+                                        opts={
+                                            {
+                                                width: '100%',
+                                                height: '100%',
+                                                playerVars: {
+                                                    autoplay: 1,
+                                                    controls: 0,
+                                                    cc_load_policy: 0,
+                                                    fs: 0,
+                                                    iv_load_policy: 0,
+                                                    modestbranding: 0,
+                                                    rel: 0,
+                                                    showinfo: 0,
+                                                },
+                                            }
+                                        }
+                                    />
+                                    <button onClick={() => setPlaying(false)} className={"button close-video"}>Close</button>
+                                </> :
+                                <div className="center-max-size">
+                                    <div className="poster-content">
+                                        {trailer ?
+                                            <button className={"button play-video"} onClick={() => setPlaying(true)} type="button">Play Trailer</button>
+                                            : 'Sorry, no trailer was found.'}
+                                        <h1>{movie.title}</h1>
+                                        <p>{movie.overview}</p>
+                                    </div>
+                                </div>
+                            }
+                        </div>
+                        : null}
+                    <div className={"center-max-size container"}>{renderMovies()}</div>
+                </main>
+                : 'Sorry, no movies were found.'}
+        </div>
+    );
 }
- 
 
 export default App;
